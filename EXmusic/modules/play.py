b394ae0d2bc138b46bc808f92b042c734be61fec
riz-ex <90839716+riz-ex@users.noreply.github.com>
2021-09-19 17:40:29 +0700

    fixes

--- EXmusic/modules/play.py
+++ EXmusic/modules/play.py
@@ -494,21 +494,21 @@ async def play(_, message: Message):
             f"<i>{user.first_name} was banned in this group, ask admin to unban @{ASSISTANT_NAME}</i>"
         )
         return
-    text_links=None
-    #await lel.edit("🔁 **Processing..**")
+    text_links = None
+    await lel.edit("🔁 **Processing** sound..")
     if message.reply_to_message:
+        if message.reply_to_message.audio:
+            pass
         entities = []
         toxt = message.reply_to_message.text or message.reply_to_message.caption
         if message.reply_to_message.entities:
             entities = message.reply_to_message.entities + entities
         elif message.reply_to_message.caption_entities:
             entities = message.reply_to_message.entities + entities
-        urls = [entity for entity in entities if entity.type == 'url']
-        text_links = [
-            entity for entity in entities if entity.type == 'text_link'
-        ]
+        urls = [entity for entity in entities if entity.type == "url"]
+        text_links = [entity for entity in entities if entity.type == "text_link"]
     else:
-        urls=None
+        urls = None
     if text_links:
         urls = True
     user_id = message.from_user.id
